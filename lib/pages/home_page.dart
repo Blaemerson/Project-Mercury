0350215e2eb1734c6d99e9dbe39dc19bb024af44
Blaemerson <blakesavage99@gmail.com>
2022-06-05 10:43:49 -0500

    Cleaned up tile creation + make tiles properly accept/remove furntiture

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:projectmercury/pages/store_page.dart';
 import 'package:projectmercury/utils/utils.dart';
 import 'package:provider/provider.dart';
+import 'dart:math' as math;
 
 import '../models/store_item.dart';
 
@@ -10,45 +11,54 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Provider.of<List<PurchasedItem>>(context);
+    /* Provider.of<List<PurchasedItem>>(context); */
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
       body: Consumer<List<PurchasedItem>>(
         builder: (_, items, __) {
-          return Center(
-            child: Stack(
-              children: [
-                /* Position each tile correctly */
-                /* TODO: Make this clearer, and remove the hardcoded values */
-                for (double i = 0, j = 0;
-                    i < 16;
-                    i++, (i % 4 == 0) ? j += 42.426 / 2 : j += 0)
-                  Tile(
-                      tileSize: 30,
-                      offsetX: i * 42.426 / 2 - (j * 3),
-                      offsetY: i * 42.426 / 2 - (j * 5)),
-                      Column(children: [
-                for (PurchasedItem item in items)
-                  /* Create a new draggable element for each item */
-                  Draggable(
-                    data: item,
-                    /* TODO: change representation of purchased furniture from their icon */
-                    child: Icon(
-                      IconData(item.icon, fontFamily: 'MaterialIcons'),
-                      size: 50,
-                    ),
-                    feedback: Icon(
-                      IconData(item.icon, fontFamily: 'MaterialIcons'),
-                      size: 70,
-                    ),
-                    childWhenDragging: Container(),
+          return Stack(
+            fit: StackFit.expand,
+            children: [
+              Center(
+                child: Transform(
+                  alignment: AlignmentDirectional.center,
+                  transform: Matrix4.rotationX(math.pi / 3),
+                  child: Transform(
+                    transform: Matrix4.rotationZ(math.pi / 4),
+                    alignment: AlignmentDirectional.center,
+                    child: GridView.count(
+                        crossAxisCount: 5,
+                        primary: false,
+                        shrinkWrap: true,
+                        children: [
+                          for (int i = 0; i < 25; i++) const Tile(),
+                        ]),
                   ),
-
-                      ],)
-              ],
-            ),
+                ),
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  for (PurchasedItem item in items)
+                    /* Create a new draggable element for each item */
+                    Draggable(
+                      data: item,
+                      /* TODO: change representation of purchased furniture from their icon */
+                      child: Icon(
+                        IconData(item.icon, fontFamily: 'MaterialIcons'),
+                        size: 50,
+                      ),
+                      feedback: Icon(
+                        IconData(item.icon, fontFamily: 'MaterialIcons'),
+                        size: 50,
+                      ),
+                      childWhenDragging: Container(),
+                    ),
+                ],
+              )
+            ],
           );
         },
       ),
@@ -79,16 +89,7 @@ class HomePage extends StatelessWidget {
 
 // TODO: See if this must be stateful
 class Tile extends StatefulWidget {
-  const Tile(
-      {Key? key,
-      required this.tileSize,
-      required this.offsetX,
-      required this.offsetY})
-      : super(key: key);
-
-  final double tileSize;
-  final double offsetX;
-  final double offsetY;
+  const Tile({Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _TileState();
@@ -96,58 +97,68 @@ class Tile extends StatefulWidget {
 
 class _TileState extends State<Tile> {
   bool _occupied = false;
-  String _occupyingFurniture = '';
-  int _tileRotation = 0;
   PurchasedItem _furniture = PurchasedItem();
 
   @override
   Widget build(BuildContext context) {
-    Color _color = Colors.lightBlueAccent;
-    return Transform.translate(
-      offset: Offset(widget.offsetX, widget.offsetY),
-      child: Transform.rotate(
-        angle: 3.1415 / 4,
-        child: DragTarget(
-          builder: ((context, candidateData, rejectedData) {
-            return Container(
-              height: widget.tileSize,
-              width: widget.tileSize,
-              /* color: Colors.grey, */
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.blueAccent),
-                color: _color,
-              ),
-              child:
-                  Icon(IconData(_furniture.icon, fontFamily: 'MaterialIcons')),
-            );
-          }),
-          onLeave: (PurchasedItem? data) {
-            _color = Colors.lightBlueAccent;
-          },
-          onWillAccept: (PurchasedItem? data) {
-            /* TODO: Only accept furniture if there is space for it on the floor. */
-            _color = Colors.red;
-            return true;
-          },
-          onAccept: (PurchasedItem data) {
-            setState(() {
-              if (_occupied == false) {
-                showSnackBar(
-                    'Added ${data.name.toLowerCase()} to home.', context);
-
-                _furniture = data;
-                _occupied = true;
-                _occupyingFurniture = data.name;
-              } else {
-                showSnackBar('Failed', context);
-                _furniture = PurchasedItem();
-                _occupied = false;
-                _occupyingFurniture = '';
-              }
-            });
-          },
-        ),
-      ),
+    Color _color = const Color.fromARGB(100, 200, 200, 200);
+    return DragTarget(
+      builder: ((context, candidateData, rejectedData) {
+        return Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            color: _color,
+          ),
+          child: Transform(
+            alignment: AlignmentDirectional.center,
+            transform: Matrix4.rotationZ(-math.pi / 4),
+            child: Transform(
+              alignment: AlignmentDirectional.center,
+              transform: Matrix4.rotationY(math.pi / 3),
+              child: _occupied
+                  ? Draggable(
+                      data: _furniture,
+                      /* TODO: change representation of purchased furniture from their icon */
+                      child: Icon(
+                        IconData(_furniture.icon, fontFamily: 'MaterialIcons'),
+                        size: 50,
+                      ),
+                      feedback: Icon(
+                        IconData(_furniture.icon, fontFamily: 'MaterialIcons'),
+                        size: 50,
+                      ),
+                      childWhenDragging: Container(),
+                      onDragStarted: () {
+                        print('Picked up ${_furniture.name}');
+                        _occupied = false;
+                      },
+                    )
+                  : Container(),
+            ),
+          ),
+        );
+      }),
+      onLeave: (PurchasedItem? data) {
+        _color = const Color.fromARGB(100, 200, 200, 200);
+      },
+      onWillAccept: (PurchasedItem? data) {
+        /* TODO: Only accept furniture if there is space for it on the floor. */
+        _color = Colors.grey;
+        return true;
+      },
+      onAccept: (PurchasedItem data) {
+        setState(() {
+          if (_occupied == false) {
+            _furniture = data;
+            _occupied = true;
+            print('Added ${data.name} to tile.');
+            /* showSnackBar('Added ${data.name.toLowerCase()} to home. (Occupied: $_occupied)', context); */
+          } else {
+            print(
+                'Furniture not added to tile; tile is occupied by ${_furniture.name}');
+          }
+        });
+      },
     );
   }
 }
